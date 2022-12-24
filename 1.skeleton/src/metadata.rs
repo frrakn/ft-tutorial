663959823d66cdd3a4e2d9b08f88642a5d2525ed
Frank Chen <cheng.frank.chen@gmail.com>
2022-12-24 15:28:38 -0500

    part 1

--- 1.skeleton/src/metadata.rs
+++ 1.skeleton/src/metadata.rs
@@ -1,16 +1,19 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::Base64VecU8;
+use near_sdk::near_bindgen;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{near_bindgen};
 
 use crate::*;
 
 #[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct FungibleTokenMetadata {
-    /*
-        FILL THIS IN
-    */
+    pub spec: String,
+    pub name: String,
+    pub icon: Option<String>,
+    pub reference: Option<String>,
+    pub reference_hash: Option<Base64VecU8>,
+    pub decimals: u8,
 }
 
 pub trait FungibleTokenMetadataProvider {
@@ -21,9 +24,6 @@ pub trait FungibleTokenMetadataProvider {
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for Contract {
     fn ft_metadata(&self) -> FungibleTokenMetadata {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        self.metadata.get().unwrap()
     }
 }
